5bf6c247a3efa2eee541fb367c6125a83a3b829e
Charly <charly.martin.1207@gmail.com>
2018-11-05 11:03:30 -0500

    airtable plugin + package.lock deleted

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,19 +2,11 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Navbar = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <h1 style={{ margin: 0 }}>
-      <Link to="/">
-        {siteTitle}
-      </Link>
-    </h1>
-
-  </div>
+  <h1>
+    <Link to="/">
+      {siteTitle}
+    </Link>
+  </h1>
 )
 
 export default Navbar
