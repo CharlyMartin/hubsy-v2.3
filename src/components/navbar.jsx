5df22530fd81efbb5672a5a0a4aaff4cc2b5612a
Charly <charly.martin.1207@gmail.com>
2018-11-08 12:21:54 -0500

    navbar in class component for interactivity

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 import { StaticQuery, graphql } from "gatsby";
 // import { Link } from 'gatsby'
 
-function extractObject(array, lang = 'fr') {
-  // Components are called internally during the build sequence,
-  // making locale = undefined which returns an empty object and fail the build.
-  // The default params 'fr' prevents that!
-  // console.log(array, lang);
-  return array.filter(obj => obj.node.data.language === lang);
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  extractObject(array, lang = 'fr') {
+    // Components are called internally during the build sequence,
+    // making locale = undefined which returns an empty object and fail the build.
+    // The default params 'fr' prevents that!
+    // console.log(array, lang);
+    return array.filter(obj => obj.node.data.language === lang);
+  }
+  
+  render() {
+    const array = this.props.data.allAirtable.edges;
+    const obj = this.extractObject(array, this.props.locale);
+    const content = obj[0].node.data;
+
+    return (
+      <div>
+        <h1>This is a Navbar in {content.language}</h1>
+        <ul>
+          <li>{content.venues}</li>
+          <li>{content.booking}</li>
+          <li>{content.pricing}</li>
+          <li>{content.blog}</li>
+        </ul>
+      </div>
+    )
+  }
 }
+
 
 export default (props) => {
   return (<StaticQuery
@@ -39,12 +64,8 @@ export default (props) => {
     }
 
     render={(data) => {      
-      const array = data.allAirtable.edges;
-      const obj = extractObject(array, props.locale);
-      const content = obj[0].node.data;
-
       return (
-        <h1>This is a Navbar in {content.language}</h1>
+        <Navbar data={data} locale={props.locale} />
       )}
     }
   />)
@@ -84,3 +105,11 @@ export default (props) => {
 // }
 
 // export default Navbar;
+
+// function extractObject(array, lang = 'fr') {
+//   // Components are called internally during the build sequence,
+//   // making locale = undefined which returns an empty object and fail the build.
+//   // The default params 'fr' prevents that!
+//   // console.log(array, lang);
+//   return array.filter(obj => obj.node.data.language === lang);
+// }
