ee8e51e28027195de82745452e499779d0159ef4
Charly <charly.martin.1207@gmail.com>
2018-12-14 19:02:42 +0100

    404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -14,7 +14,7 @@ class NotFoundPage extends React.Component {
           <div className="full-page">
             <div className="container">
               <h1>Cette page n'existe plus 😬</h1>
-              <p>Et un jour toi aussi, tout comme cette page, tu n'existeras plus…  Ton temps est limité, ne le gâche sur des choses futiles. Tu dois bien avoir des rêves dans la vie non !? Tu sais ce road-trip en Australie avec tes potes, ou cet start-up dont tu parles à tout le monde depuis 3 ans.</p>
+              <p>Et un jour toi aussi, tout comme cette page, tu n'existeras plus…  Ton temps est limité, ne le gâche sur des choses futiles. Tu dois bien avoir des rêves dans la vie non !? Tu sais comme ce road-trip en Australie avec tes potes, ou cette start-up dont tu parles à tout le monde depuis 3 ans.</p>
               <p>Ben alors, qu'est ce que tu attends ? Allez, attrape ton ordi, tes écouteurs et ton carnet de note. Passe chez Hubsy et commence à réaliser tes rêves. Avec du bon café, tout est possible 🙂</p>
               <p>Clique sur le bouton et trouve un espace proche de chez toi. On t'attend !</p>
 
