448f4c9039e5d9bdb6fb219274df6b64334ec53b
Charly <charly.martin.1207@gmail.com>
2019-02-21 12:33:55 +0700

    Fixing bug about carousel crashing

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -9,18 +9,30 @@ import '../css/components/alert.css'
 class Alert extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {visible: true}
   }
 
-  componentDidMount() {
-    closeAlert();
+  // componentDidMount() {
+  //   closeAlert();
+  // This is removing an alert
+  // but react does not know that because it's using document.addeventListener
+  // Hence the previous bug
+  // }
+
+  close() {
+    this.setState({ visible: false });
   }
 
   render() {
+    // Returns nothing if visible state is false
+    if (!this.state.visible) { return null }
+
     return (
       <div className="alert">
         <div className="container">
           <p>{this.props.content}</p>
-          <div className="cross-container">
+          <div className="cross-container" onClick={this.close.bind(this)}>
             <Image src={cross_white}/>
           </div>
         </div>
